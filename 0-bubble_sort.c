13d892815605bf80f10e7f1468191e9d98a17006
Chaimaazerouali <zeroualiiichaimaaa@gmail.com>
2023-12-23 14:23:18 -0800

    python

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * bb_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i = 0;
+	int bb = 1, tp;
+
+	if (!array || size < 2)
+		return;
+	while (bb)
+	{
+		bb = 0;
+		for (i = 0; i < (size - 1) && i != (size - 1); i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				tp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tp;
+				bb = 1;
+				print_array(array, size);
+			}
+		}
+		i = 0;
+	}
+}
